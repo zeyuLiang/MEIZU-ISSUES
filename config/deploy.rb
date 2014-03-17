ad0a2ef24793ee911053576ca0764d8099533328
zeyu <zeyu@meizu.com>
2014-03-17 17:29:54 +0800

    add deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,3 @@
-set :stages, %w(staging production)
+set :stages, %w(staging production redmine_production)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
---
+++ config/deploy/redmine_production.rb
@@ -0,0 +1,18 @@
+set :repository, "git://github.com/khonsou/MEIZU-ISSUES.git"
+set :domain, "172.16.201.15"
+set :application_servers, %w(172.16.201.15)
+set :migration_server, "172.16.201.15"
+set :user, "rails"
+set :rails_env, "production"
+set :branch, "redmine_production"
+# set :deploy_via, :remote_cache
+set :application, "plano"
+#ssh_options[:port] = 16120 
+
+load File.dirname(__FILE__) + "/shared_code"
+
+task :assets_precompile, :roles => [:app] do
+  run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile 2>/dev/null"
+end
+
+after "deploy:update_code", :assets_precompile
