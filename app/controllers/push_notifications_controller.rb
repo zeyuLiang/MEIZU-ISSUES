07c85fa7b10219b873bd4fc4ad4e8a2ef94ce595
zeyu <zeyu@meizu.com>
2014-03-20 14:34:39 +0800

    modify the popver of  pushnotifications and memberinvitations

--- app/controllers/push_notifications_controller.rb
+++ app/controllers/push_notifications_controller.rb
@@ -23,18 +23,12 @@ class PushNotificationsController < ApplicationController
   end
   
   def mark_popover_as_read
-    pending_members_push_notifications_and_unread_count
-    if @pending_members.size > 10
-      push_notification_member_pending = 10
-    else
-      pending_members_count = @pending_members.size
-    end
-    push_notifications = User.current.push_notifications.unread.limit(10 - pending_members_count)
+    push_notifications = User.current.push_notifications.unread.limit(10)
     push_notifications.each do |push_notification|
       push_notification.mark_as_read
     end
-    @push_notifications = User.current.push_notifications.where("pusher_type != 'MemberInvitation'").unread
-    @unread_count = @push_notifications.count + @pending_members.size
+    @push_notifications = User.current.push_notifications.unread
+    @unread_count = @push_notifications.count
   end
 
   def mark_all_as_read
@@ -46,17 +40,11 @@ class PushNotificationsController < ApplicationController
   private
 
   def pending_members_push_notifications_and_unread_count
-    member_invitations = User.current.member_invitations.where("state = 'pending'")\
-     - User.current.member_invitations.where(:project_id => User.current.members.map{|p| p.project_id})
-    @push_notifications = User.current.push_notifications.where("pusher_type != 'MemberInvitation'").unread
-    @pending_members = member_invitations.map{|p| p.push_notifications}.reverse.flatten
-    @unread_count = @push_notifications.count + @pending_members.size
+    @push_notifications = User.current.push_notifications.unread
+    @unread_count = @push_notifications.count
    
     @changes = session[:notifications].nil? ? @push_notifications.map{|notification| notification.id} : \
 			      (@push_notifications.map{|notification| notification.id} - session[:notifications])
-    @changes += session[:pending_members].nil? ? @pending_members.map{|member| member.id} : \
-			      (@pending_members.map{|member| member.id} - session[:pending_members])
     session[:notifications] = @push_notifications.map{|notification| notification.id}
-    session[:pending_members] = @pending_members.map{|member| member.id}
   end
 end
